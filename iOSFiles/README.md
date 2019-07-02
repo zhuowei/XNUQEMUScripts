70100d85743ec44fba64ac408191981d05d0779a
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2019-07-02 00:44:22 -0700

    Load trustcache, move iOS files into separate folder

---
+++ iOSFiles/README.md
@@ -0,0 +1,11 @@
+You should have 4 files here:
+
+devicetree.dtb
+
+Generated with `python3 modifydevicetree.py path/to/ipsw/Firmware/all_flash/DeviceTree.d321ap.im4p devicetree.dtb`
+
+kcache_out.bin
+ramdisk.dmg
+trustcache.bin
+
+Generated from the ipsw by following [Aleph Security's guide](https://alephsecurity.com/2019/06/17/xnu-qemu-arm64-1/).
--- runqemu.sh
+++ runqemu.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
+export QEMU_TRUSTCACHE=iOSFiles/trustcache.bin
 qemu/build-aarch64/aarch64-softmmu/qemu-system-aarch64 -M virt -cpu max \
-	-kernel kcache_out.bin \
-	-dtb devicetree.dtb \
+	-kernel iOSFiles/kcache_out.bin \
+	-dtb iOSFiles/devicetree.dtb \
 	-monitor stdio \
 	-m 2G -s -S -d unimp \
 	-serial file:/dev/stdout \
 	-serial file:/dev/stdout \
 	-serial file:/dev/stdout \
 	-append "debug=0x8 kextlog=0xfff cpus=1 rd=md0" \
-	-initrd ramdisk.dmg $@
+	-initrd iOSFiles/ramdisk.dmg $@
