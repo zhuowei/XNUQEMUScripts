27f97614f4c816f766f7de4cc3086ba061a8c98b
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2019-07-02 02:01:54 -0700

    Almost enable serial input (again, thanks to Aleph Security)
    Doesn't seem to work yet?

--- runqemu.sh
+++ runqemu.sh
@@ -3,10 +3,9 @@ export QEMU_TRUSTCACHE=iOSFiles/trustcache.bin
 qemu/build-aarch64/aarch64-softmmu/qemu-system-aarch64 -M virt -cpu max \
 	-kernel iOSFiles/kcache_out.bin \
 	-dtb iOSFiles/devicetree.dtb \
-	-monitor stdio \
-	-m 2G -s -S -d unimp \
-	-serial file:/dev/stdout \
-	-serial file:/dev/stdout \
-	-serial file:/dev/stdout \
-	-append "debug=0x8 kextlog=0xfff cpus=1 rd=md0" \
+	-m 3G -d unimp \
+	-serial file:/dev/null \
+	-serial file:/dev/null \
+	-serial mon:stdio \
+	-append "debug=0x8 kextlog=0xfff cpus=1 rd=md0 serial=0x2" \
 	-initrd iOSFiles/ramdisk.dmg $@
